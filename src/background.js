5ac8e9309651dfe7c9a5e883d9937dda4fbd1c16
NegativeDearc <datingwithme@live.cn>
2018-11-23 15:49:15 +0800

    - sync with web version

--- src/background.js
+++ src/background.js
@@ -1,70 +1,105 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import path from 'path'
+import {app, protocol, Tray, BrowserWindow, Menu} from 'electron'
 import {
-  createProtocol,
-  // installVueDevtools
+    createProtocol,
+    // installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+let appTray
+let trayClose
 
 // Standard scheme must be registered before the app is ready
-protocol.registerStandardSchemes(['app'], { secure: true })
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 970, height: 800 })
+protocol.registerStandardSchemes(['app'], {secure: true})
 
-  if (isDevelopment) {
-    // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadFile('index.html')
-  }
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({width: 970, height: 800})
 
-  win.on('closed', () => {
-    win = null
-  })
+    if (isDevelopment) {
+        // Load the url of the dev server if in development mode
+        win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+        if (!process.env.IS_TEST) win.webContents.openDevTools()
+    } else {
+        createProtocol('app')
+        // Load the index.html when not in development
+        win.loadFile('index.html')
+    }
+
+    win.on('close', () => {
+        if (process.platform === 'win') {
+            win.hide()
+            const iconPath = path.join(__dirname, '/public/favicon.ico')
+            appTray = new Tray(iconPath)
+            // 图标的上上下文
+            const contextMenu = Menu.buildFromTemplate(trayMenuTemplate)
+            // 设置此托盘图标的悬停提示内容
+            appTray.setToolTip('天天好心情')
+            // 设置此图标的上下文菜单
+            appTray.setContextMenu(contextMenu)
+            // 主窗口显示隐藏切换
+            appTray.on('click', () => {
+                win.isVisible() ? win.hide() : win.show()
+            })
+            event.preventDefault()
+        }
+    })
+
+    win.on('closed', () => {
+        win = null
+    })
+
+    const trayMenuTemplate = [
+        {
+            label: '退出',
+            click: function () {
+                // 退出
+                trayClose = true
+                app.quit()
+            }
+        }
+    ]
 }
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  // if (isDevelopment && !process.env.IS_TEST) {
-  //   // Install Vue Devtools
-  //   await installVueDevtools()
-  // }
-  createWindow()
+    // if (isDevelopment && !process.env.IS_TEST) {
+    //   // Install Vue Devtools
+    //   await installVueDevtools()
+    // }
+    createWindow()
 })
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  process.on('message', data => {
-    if (data === 'graceful-exit') {
-      app.quit()
-    }
-  })
+    process.on('message', data => {
+        if (data === 'graceful-exit') {
+            app.quit()
+        }
+    })
 }
